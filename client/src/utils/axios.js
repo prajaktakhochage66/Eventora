c049cdd4956c7ec470f01f52a5e7d1c1b0832415
prajaktakhochage66 <138974607+prajaktakhochage66@users.noreply.github.com>
2026-07-17 19:58:26 +0530

    Updated axios.jsx

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://eventora-backend-ed6f.onrender.com',
+    baseURL: 'https://eventora-backend-ed6f.onrender.com/api',
 });
 
 api.interceptors.request.use((config) => {
